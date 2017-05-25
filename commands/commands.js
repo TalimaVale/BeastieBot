a40b2eb3ddd8b2d16af8db05c53d50040a95b54a
Ship of Genius <shipofgenius@gmail.com>
2017-05-25 02:23:34 -0600

    custom commands
    custom messages can now be fetched by !commandName

--- commands/commands.js
+++ commands/commands.js
@@ -6,6 +6,7 @@
 const _ = require("lodash");
 const beastieFunctions = require("../beastie-functions");
 const queue = require("../message-queue");
+const fs = require("fs");
 
 // Setup Files
 const beastie = require("../beastie-client");
@@ -15,6 +16,14 @@ const secrets = require("../config/secrets");
 const clearance = require("./clearance");
 const raid = require("./raids");
 const custom = require("./custom");
+
+// Custom Command vars
+var  contents = fs.readFileSync("./commands/custom.json");
+var jsonObj = JSON.parse(contents);
+var names = _.map(jsonObj.commands, "name");
+console.log(names);
+var customCom  = {};
+var name;
 
 // Helper function for responding to the user with a generic/non-interactive reply in the format of "@{username}, {text}".
 // Usage: 
@@ -29,35 +38,36 @@ const reply = (text) => clearance.viewer((channel, userstate) => {
 /**
  * Beastie's command chart
  *
- * viewer commands          description                                  file
- * ------------------------ -------------------------------------------- -----------------------
- * !twitter                 Beastie links to Twitter                     /commands/commands.js
- * !patreon                 Beastie links to Patreon                     /commands/commands.js
- * !github                  Beastie links to GitHub                      /commands/commands.js
- * !gitlab                  Beastie links to GitLab                      /commands/commands.js
- * !discord                 Beastie links to Discord                     /commands/commands.js
- * !instagram               Beastie links to Instagram                   /commands/commands.js
- * !youtube                 Beastie links to YouTube                     /commands/commands.js
- * !teamsite                Beastie links to Team Site                   /commands/commands.js
- * !teamwall                Beastie links to Team Wall                   /commands/commands.js
+ * viewer commands            description                                  file
+ * ------------------------  -------------------------------------------- -----------------------
+ * !twitter                   Beastie links to Twitter                     /commands/commands.js
+ * !patreon                   Beastie links to Patreon                     /commands/commands.js
+ * !github                    Beastie links to GitHub                      /commands/commands.js
+ * !gitlab                    Beastie links to GitLab                      /commands/commands.js
+ * !discord                   Beastie links to Discord                     /commands/commands.js
+ * !instagram                 Beastie links to Instagram                   /commands/commands.js
+ * !youtube                   Beastie links to YouTube                     /commands/commands.js
+ * !teamsite                  Beastie links to Team Site                   /commands/commands.js
+ * !teamwall                  Beastie links to Team Wall                   /commands/commands.js
  *                                                                       
- * !goodbyebeastie          Beastie says 'goodbye' to user               /commands/commands.js
- * !hellobeastie            Beastie says 'hello' to user                 /commands/commands.js
- * !helpbeastie             Beastie posts list of commands in chat       /commands/commands.js
- * !pet                     Viewer /me pets Beastie                      /commands/commands.js
- * !raidready               Viewers join raidTeam                        /commands/commands.js
- * !rawr                    Beastie leaves 'rawr' message                /commands/raids.js
- * !uptime                  Beastie posts uptime in chat                 /commands/commands.js
+ * !goodbyebeastie            Beastie says 'goodbye' to user               /commands/commands.js
+ * !hellobeastie              Beastie says 'hello' to user                 /commands/commands.js
+ * !helpbeastie               Beastie posts list of commands in chat       /commands/commands.js
+ * !pet                       Viewer /me pets Beastie                      /commands/commands.js
+ * !raidready                 Viewers join raidTeam                        /commands/commands.js
+ * !rawr                      Beastie leaves 'rawr' message                /commands/raids.js
+ * !uptime                    Beastie posts uptime in chat                 /commands/commands.js
  *
- * moderator commands       description                                  file
- * ------------------------ -------------------------------------------- -----------------------
- * !flushqueue              Beastie empties message queue                /commands/commands.js
- * !raidteam                Moderator checks the raid team               /commands/raids.js
- * !shoutout <username>     Beastie shouts out a twitch channel          /commands/commands.js
+ * moderator commands           description                                  file
+ * ------------------------     -------------------------------------------- -----------------------
+ * !flushqueue                  Beastie empties message queue                /commands/commands.js
+ * !raidteam                    Moderator checks the raid team               /commands/raids.js
+ * !shoutout <username>         Beastie shouts out a twitch channel          /commands/commands.js
  *
- * broadcaster commands     description                                  file
- * ------------------------ -------------------------------------------- -----------------------
- * !raidstart               Broadcaster prepares to raid                 /commands/raids.js
+ * broadcaster commands         description                                  file
+ * ------------------------     -------------------------------------------- -----------------------
+ * !raidstart                   Broadcaster prepares to raid                 /commands/raids.js
+ * !commands add !command msg   adds custom command named [!command] with message [msg]
  *
  */
 
@@ -167,6 +177,7 @@ var commands = {
                             var str = message;
                             // send to custom.js comHandler
                             custom.comHandler(str);
+                            mergeCommands();                            
                            // queue.addMessage(channel, "");
                         }),
                         
@@ -181,33 +192,43 @@ var commands = {
 
 // *** Build object of custom commands and attach it to commands object. I seriously need to seriously refactor this for serious-SoG *** //
 // TODO - Move this to custom.js
-var jsonObj = require("./custom.json");
-var names = _.map(jsonObj.commands, "name");
-var customCom  = {};
-var name;
+
+
 
 // iterate names and messages by index[i]. 
 for(var i=0;i < names.length;i++){
     name = names[i];
+    console.log(name);
     //build array of custom commands
     customCom[name.replace("!","")] = clearance.broadcaster(
             // function in addMessage parameter fetches message of custom command when clearance.broadcaster() is called.
         function(channel,userstate,message){queue.addMessage(channel,getM(message));});
  }
  
+ // fetches the message for a custom command
 function getM(command)
 {
-    console.log(jsonObj.commands);
+ 
+    // grab the index of command
     var index = _.findIndex(jsonObj.commands, function(c) { return c.name == command; });
+    // return the message at index
     return jsonObj.commands[index].message;
     }
 // **************************************************************************************************************************************** //
 
-
-// Assigns all of the raid.commands to 
+//merges all command objects to commands
+function mergeCommands(){
+        
+    console.log("mergeCommands was called: " + jsonObj);
+// Assigns all of the raid.commands to commands
 Object.assign(commands, raid.commands);
  // merge custom commands to commands 
 _.merge(commands, customCom);
+}
 
+
+
+// make magic happen
+mergeCommands();
 module.exports = commands;
 
